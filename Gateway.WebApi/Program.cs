f40b86a663e5e36f56d58547307e49154d843593
Sudipta Bhowmick <sudipta.bhowmick@valmetpartners.com>
2025-05-09 15:37:22 +0600

    Implemented API Gateway using Ocelot, added Postman collection for testing

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -1,13 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 namespace Gateway.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot();
+
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            //Use Ocelot middleware
+            await app.UseOcelot();
 
             app.Run();
         }
